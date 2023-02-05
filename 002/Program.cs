be4bc113d7da67c8ac14976ca389c7c7ee3a9e2c
Nikita <potapov.nikita1997@gmail.com>
2023-02-05 14:44:07 +0300

    New project

---
+++ 002/Program.cs
@@ -0,0 +1,8 @@
+﻿// 2. С клавиатуры вводятся два вещественных числа. Проверять является ли одно из них квадратом второго
+
+double a,b;
+string s;
+s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
