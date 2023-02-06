517c689c51bbe4ce5071140909332f37221d32f7
Nikita <potapov.nikita1997@gmail.com>
2023-02-06 22:21:28 +0300

    FourSeminar2

---
+++ 040/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задать массив из 8 целых элементов и вывести их на экран
+
+int[] a;
+a=new int[8];
+Random random=new Random();//Описание и создниае объекта
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(0,10);
+
+for(int i=0;i<a.Length;i++)
+    
---
+++ 049/Program.cs
@@ -0,0 +1,19 @@
+﻿// В Указанном массиве целых чисел найдите разницу между максимальным и минимальным элементом
+
+int[] a=new int[6];
+Random random=new Random();
+for(int i=0;i<a.Length;i++)
+    a[i]=random.Next(6,10);
+
+for(int i=0;i<a.Length;i++)
+    System.Console.Write($"{a[i]} ");
+System.Console.WriteLine();
+
+int max=a[0];
+for(int i=1;i<a.Length;i++)
+    if (a[i]>max)
+        max=a[i];
+
+
+
+System.Console.WriteLine(max);
