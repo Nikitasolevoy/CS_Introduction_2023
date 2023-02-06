7bab52006c89dedbb75de37539a701a0f18c8e7e
Nikita <potapov.nikita1997@gmail.com>
2023-02-06 21:29:02 +0300

    FourSeminar

---
+++ Array/Program.cs
@@ -0,0 +1,26 @@
+﻿//Массивы
+
+//Подсчитать среднюю температуру за неделю
+//double t1,t2,t3,t4,t5,t6,t7;
+//t1=Convert.ToDouble(Console.ReadLine());
+//t2=Convert.ToDouble(Console.ReadLine());
+//t3=Convert.ToDouble(Console.ReadLine());
+//t4=Convert.ToDouble(Console.ReadLine());
+//t5=Convert.ToDouble(Console.ReadLine());
+//t6=Convert.ToDouble(Console.ReadLine());
+//t7=Convert.ToDouble(Console.ReadLine());
+//double sr=(t1+t2+t3+t4+t5+t6+t7);
+//System.Console.WriteLine(sr);
+
+//Инизилиазиация массива
+double [] temps=new double [7];
+//Ввод данных
+for(int i=0;i<temps.Length;i++)
+    temps[i]=Convert.ToDouble(Console.ReadLine());
+//Решение задачи
+//находим сумму массива
+double s=0;
+for(int i=0;i<temps.Length;i++)
+    s=s+temps[i];
+//Вывод данных
+System.Console.WriteLine(s/temps.Length);
---
+++ Swap/Program.cs
@@ -0,0 +1,67 @@
+﻿//void Swap(int x, int y)//Передача по значентю (Value)
+//{
+  //  int t=x;
+    //x=y;
+    //y=t;
+
+//}
+
+void Swap(ref int x,ref int y)//Передача по значентю (referecne) ref- модификатор аргументов
+{
+    int t=x;
+    x=y;
+    y=t;
+
+}
+void Calculator(int a,int b,out int sum,out int sub)
+{
+    sum=a+b;
+    sub=a-b;
+}
+
+//int u,r=0,o=0;
+//u=r+o;
+
+int a=4;
+int b=5;
+int s=0,sb=0;
+Calculator(a,b,out s,out sb);
+
+
+System.Console.WriteLine($"a={a} b={b}");
+//Классический обмен
+Swap(ref a,ref b);
+Console.WriteLine($"a={a} b={b}");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Хитрый обменя
+
+//a=a+b;
+//b=a-b;
+//a=a-b;
